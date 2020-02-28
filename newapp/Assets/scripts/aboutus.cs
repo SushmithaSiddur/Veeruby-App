be87f404ebbbe2ffc0b5520143cf006877befe5e
SushmithaSiddur <58904663+SushmithaSiddur@users.noreply.github.com>
2020-02-28 11:41:06 +0530

    First step

---
+++ newapp/Assets/scripts/aboutus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class aboutus : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public void LoadLevel()
+    {
+
+        SceneManager.LoadScene(2);  //index 1
+    }
+
+}
---
+++ newapp/Assets/scripts/newUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class newUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+/**
+    public GameObject img;
+     public GameObject img2;
+
+     public void buttonEvent()
+     {
+         if (img.active)
+             img.SetActive(false);
+         else
+             img.SetActive(true);
+     }
+     public void buttonEvent1()
+     {
+         if (img2.active)
+             img2.SetActive(false);
+         else
+             img2.SetActive(true);
+     }
+     
+  **/
+    
+    public void LoadLevel()
+    {
+        
+        SceneManager.LoadScene(1);  //index 1
+    }
+
+}
